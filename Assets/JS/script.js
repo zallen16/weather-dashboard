eae90c6e31859a41dcefc0736c645340b9137800
zallen16 <126983111+zallen16@users.noreply.github.com>
2023-12-11 23:54:13 -0500

    added APIs, added basic js functions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,2 +1,34 @@
 var APIkey = '3f3790c4a7515348da4eb07b739d35d4';
+// var coordinatesAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=Miami&limit=1&appid=3f3790c4a7515348da4eb07b739d35d4';
+var fiveDayAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=' + APIkey;
+// fetch(fiveDayAPI)
+// .then((response)=>{
+//     console.log(response)
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data)
+
+// })
+// fetch(coordinatesAPI)
+// .then((response)=>{
+//     console.log(response)
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data)
+// })
+function getCoordinates (cityName){
+    console.log(cityName)
+    var coordinatesAPI = 'http://api.openweathermap.org/geo/1.0/direct?q='+cityName+'&limit=1&appid=3f3790c4a7515348da4eb07b739d35d4';
+    fetch(coordinatesAPI)
+    .then((response)=>{
+        console.log(response)
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data)
+    })
+}
+getCoordinates("New York")
 
